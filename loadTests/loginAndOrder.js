ea0ee72328f29f8e0901778ef1ffe291627f153f
tesessa <tessaeliseandersen@yahoo.com>
2025-03-31 15:11:21 -0600

    Adding load test

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,157 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+import jsonpath from 'https://jslib.k6.io/jsonpath/1.0.2/index.js'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  const vars = {}
+
+  group('page_1 - https://pizza.tesessa.click/', function () {
+    // Login
+    response = http.put(
+      'https://pizza-service.tesessa.click/api/auth',
+      '{"email":"d@jwt.com","password":"diner"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.tesessa.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    
+    if(!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log(response.body);
+      fail('Login was *not* 200');
+    }
+
+    vars['token'] = jsonpath.query(response.json(), '$.token')[0]
+
+    sleep(9)
+
+    // Get menu
+    response = http.get('https://pizza-service.tesessa.click/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        'if-none-match': 'W/"1fc-cgG/aqJmHhElGCplQPSmgl2Gwk0"',
+        origin: 'https://pizza.tesessa.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(0.6)
+
+    // Get franchises
+    response = http.get('https://pizza-service.tesessa.click/api/franchise', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        authorization: `Bearer ${vars['token']}`,
+        'content-type': 'application/json',
+        'if-none-match': 'W/"40-EPPawbPn0KtYVCL5qBynMCqA1xo"',
+        origin: 'https://pizza.tesessa.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(11.6)
+
+    // Order a pizza
+    response = http.post(
+      'https://pizza-service.tesessa.click/api/order',
+      '{"items":[{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.tesessa.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    sleep(2.2)
+
+    vars['jwt'] = jsonpath.query(response.json(), '$.jwt')[0]
+
+    // Verify jwt pizza
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      JSON.stringify({ jwt: vars['jwt'] }),
+      // {"jwt": `${vars['jwt']}`},
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: `Bearer ${vars['token']}`,
+          'content-type': 'application/json',
+          origin: 'https://pizza.tesessa.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'sec-fetch-storage-access': 'active',
+        },
+      }
+    )
+  })
+}
